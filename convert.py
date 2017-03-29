4022c89098c981063b58d4ad7f5625fc6170a332
crew1274 <crew1274@yahoo.com.tw>
2017-03-29 18:50:26 +0800

    update

--- convert.py
+++ convert.py
@@ -1,12 +1,17 @@
 import pandas as pd
-#df=pd.read_csv('../dataset/record.csv') #讀取資料
-df=pd.read_csv('../dataset/test.csv') #讀取資料
+df=pd.read_csv('../dataset/record.csv') #讀取資料
+#df=pd.read_csv('../dataset/test.csv') #讀取資料
 print(df.describe)#資料顯示
 print(df.dtypes)#資料格式
 print(df.isnull().sum()) #偵測nan
-df['index'] = pd.to_datetime(df['Date']+" "+df['Time']) #時間處理
-print(df)
-new=pd.DataFrame({'Global_active_power':df['Global_active_power']})
-new.index = df['index']
-print(new.describe)
-new.to_csv('../dataset/new_test.csv')
+df['Datetime'] = pd.to_datetime(df['Date']+" "+df['Time']) #時間處理
+del df['Date']
+del df['Time']
+del df['Global_reactive_power']
+del df['Voltage']
+del df['Global_intensity']
+df.index = df['Datetime']
+del df['Datetime']
+print(df.describe)
+#df.to_csv('../dataset/new_test.csv')
+df.to_csv('../dataset/new_record.csv')
--- read.py
+++ read.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
-df=pd.read_csv('new_record.csv') #讀取資料
-df.index = pd.to_datetime(df['Time'])
-del df['Time']
+df=pd.read_csv('../dataset/new_record.csv') #讀取資料
+df.index = pd.to_datetime(df['Datetime']) #轉換index，因為從csv讀取無index
 print(df['2007-4-1':'2007-4-2'])
