11e1b06b96fbb0de3883713ff5d26ecad142573e
crew1274 <crew1274@yahoo.com.tw>
2017-04-13 03:09:08 +0800

    update vaild function

--- extraction.py
+++ extraction.py
@@ -1,26 +1,26 @@
-import sys 
+import sys
 import pandas as pd
 import matplotlib.pyplot as plot
 import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
-def read(target_time):
+def read(target_time, gap):
     range_y=target_time
-    range_x=range_y-pd.to_timedelta(1, unit='h')    #往前抓取1小時
-    loc_pre=df.loc[range_x:range_y].Global_active_power #撈資料
+    range_x=range_y-pd.to_timedelta(gap, unit='m')   
+    loc_pre=df.loc[range_x:range_y].Global_active_power 
     loc_pre_mean= loc_pre.rolling(window=15).mean()
     loc_pre_mean[15:].plot()
     plot.title(range_y)
     plot.show()
 
-def main( target_time ):
+def main( target_time , gap ):
     #抓取時間範圍
     range_y=target_time
-    range_x=range_y-pd.to_timedelta(1, unit='h')    #往前抓取1小時
+    range_x=range_y-pd.to_timedelta(gap, unit='m')    #往前抓取1小時
     loc_pre=df.loc[range_x:range_y].Global_active_power #撈資料
     loc_pre_mean= loc_pre.rolling(window=15).mean()    #計算移動平均
-    print(loc_pre_mean[15:].values)
+    #print(loc_pre_mean[15:].values)
     print('===============================')
     min_distance = None
     stamp = None
@@ -29,17 +29,17 @@ def main( target_time ):
     for i in range(1,6,1): #遞迴5次
         #抓取時間範圍
         range_y=target_time-pd.to_timedelta(7*i, unit='d')#抓取上禮拜資料
-        range_x=range_y-pd.to_timedelta(1, unit='h') #抓取前1小時
+        range_x=range_y-pd.to_timedelta(gap, unit='m') #抓取前1小時
         range_time=range_x,range_y
         loc_before = df.loc[range_time[0]:range_time[1]].Global_active_power #抓資料
 
         #計算移動平均
         loc_before_mean= loc_before.rolling(window=15).mean()
-        print(loc_before_mean[15:].values)
+        #print(loc_before_mean[15:].values)
         #計算移動平均的DWT距離
         distance, path = fastdtw(loc_pre_mean[15:].values, loc_before_mean[15:].values, dist=euclidean)
-        print(distance)
-        print('===============================')
+        #print(distance)
+        #print('===============================')
         
         if min_distance == None:
             min_distance=distance
@@ -51,18 +51,18 @@ def main( target_time ):
     #天為單位比較相似度
     for i in range(1,4,1): #遞迴5次
         #抓取時間範圍
-        range_y=target_time-pd.to_timedelta(1*i, unit='d')#抓取上禮拜資料
-        range_x=range_y-pd.to_timedelta(1, unit='h') #抓取前1小時
+        range_y=target_time-pd.to_timedelta(1*i, unit='d')
+        range_x=range_y-pd.to_timedelta(gap, unit='m') 
         range_time=range_x,range_y
         loc_before = df.loc[range_time[0]:range_time[1]].Global_active_power #抓資料
 
         #計算移動平均
         loc_before_mean= loc_before.rolling(window=15).mean()
-        print(loc_before_mean[15:].values)
+        #print(loc_before_mean[15:].values)
         #計算移動平均的DWT距離
         distance, path = fastdtw(loc_pre_mean[15:].values, loc_before_mean[15:].values, dist=euclidean)
-        print(distance)
-        print('===============================')
+        #print(distance)
+        #print('===============================')
          
         if min_distance == None:
             min_distance=distance
@@ -70,6 +70,8 @@ def main( target_time ):
         if min_distance >  distance:
             min_distance=distance
             stamp = i
+    print('===============================')
+    print(gap)
     print(min_distance)
     print(stamp)
     return min_distance , stamp
@@ -81,9 +83,11 @@ if __name__ == "__main__":
     df.index = pd.to_datetime(df['Datetime']) #轉換index，因為從csv讀取無index
     date=format(sys.argv[1])
     time=format(sys.argv[2])
+    gap = 90 #1.5個小時
     target_time=pd.to_datetime(date+' '+time)#轉換時間標籤
+    for gap in range(30,105,15):
+        min_distance , stamp = main(target_time, gap)
 
-    min_distance , stamp = main(target_time)
-    read(target_time)
-    min_target_time=target_time-pd.to_timedelta(stamp , unit='d')
-    read(min_target_time)
+    #min_target_time=target_time-pd.to_timedelta(stamp , unit='d')
+    #read(target_time,gap)
+    #read(min_target_time,gap)
--- valid.py
+++ valid.py
@@ -6,21 +6,31 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
+def read(target_time):
+    range_x=target_time-pd.to_timedelta(15, unit='m')    
+    range_y=range_x+pd.to_timedelta(30, unit='m')   
+    loc_pre=df.loc[range_x:range_y].Global_active_power 
+    loc_pre_mean= loc_pre.rolling(window=15).mean()
+    loc_pre_mean[15:].plot()
+    plot.title(range_y)
+    plot.show()
+
 df=pd.read_csv('D:\Dropbox\paper/dataset/new_record.csv') #讀取資料
 df.index = pd.to_datetime(df['Datetime']) #轉換index，因為從csv讀取無index
 
 #日期時間
 date=format(sys.argv[1])
 time=format(sys.argv[2])
+gap=int(format(sys.argv[3]))
 target_time=pd.to_datetime(date+' '+time)#轉換時間標籤
 
 #抓取時間範圍
-range_y=target_time
-range_x=range_y-pd.to_timedelta(1, unit='h')#往前抓一小時
+range_x=target_time-pd.to_timedelta(15, unit='m')
+range_y=range_x+pd.to_timedelta(30, unit='m')#往後抓15分鐘
 loc_pre=df.loc[range_x:range_y].Global_active_power
 
-range_y=target_time-pd.to_timedelta(14, unit='d')
-range_x=range_y-pd.to_timedelta(1, unit='h')
+range_x=target_time-pd.to_timedelta( gap , unit='d')-pd.to_timedelta(15, unit='m')
+range_y=range_x+pd.to_timedelta(30, unit='m')#往後抓15分鐘
 loc_before=df.loc[range_x:range_y].Global_active_power
 
 #計算移動平均
@@ -40,3 +50,6 @@ print(distance)
 #計算移動平均的DWT距離
 distance, path = fastdtw(loc_pre_mean[15:].values, loc_before_mean[15:].values, dist=euclidean)
 print(distance)
+read(target_time)
+min_target_time=target_time-pd.to_timedelta(gap , unit='d')
+read(min_target_time)
